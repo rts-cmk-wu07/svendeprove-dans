dcf60841459a794d1f709ddf2709a45a0e9037d8
Adam <adamchatila2020@gmail.com>
2023-03-14 15:39:31 +0100

    done with aktivittet

--- app/src/components/activiteter/AktiviteterCards.js
+++ app/src/components/activiteter/AktiviteterCards.js
@@ -1,3 +1,7 @@
+
+
+
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -16,16 +20,17 @@ const AktiviteterCard = () => {
 
   return (
     <div>
-      <h1>Aktiviteter</h1>
+      <h1 className='text-white font-normal pl-9 pt-9 text-4xl'>Aktiviteter</h1>
       <div className="flex flex-wrap justify-center">
         {activities.map((activity) => (
-          <div key={activity.id} className="max-w-sm rounded overflow-hidden shadow-lg m-4">
-            <img src={activity.image} alt={activity.title} className="w-full h-48 object-cover object-center" />
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">{activity.title}</div>
-              <p className="text-gray-700 text-base">{activity.description}</p>
+            <div key={activity.id} className="relative w-[356px] h-[344px] rounded-l-[40px] rounded-tr-[40px] overflow-hidden shadow-lg m-4">
+            <img src={activity.asset.url} alt={activity.url} className="w-full h-full object-cover" />
+            <div className="absolute bottom-0 w-full px-6 py-4 bg-purple-500 opacity-80 z-10 h-[96px]">
+              <div className="font-bold text-white text-xl mb-2 ">{activity.name}</div>
+              <p className="text-white text-base">{activity.minAge}-{activity.maxAge} år</p>
             </div>
           </div>
+
         ))}
       </div>
     </div>
