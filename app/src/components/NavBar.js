4f6bb8fb188e4663710ac6dd63a103f89cfa069d
Adam <adamchatila2020@gmail.com>
2023-03-20 15:34:53 +0100

    hey end

--- app/src/components/NavBar.js
+++ app/src/components/NavBar.js
@@ -7,16 +7,16 @@ const NavBar = () => {
     <div className="z-50 fixed bottom-0 w-full">
       <div className="bg-[#E9E9E9] h-[56px]">
         <div className="flex items-center justify-around pt-2 ">
-          <Link to={"/aktiviteter"} className="border border-black rounded-full p-2 hover:bg-[#5E2E53]">
+          <Link to={"/aktiviteter"} className="border border-black rounded-full p-2 hover:bg-white">
             <AiOutlineHome className="text-black text-2xl" />
           </Link>
-          <Link to={"/search"} className="border border-black rounded-full p-2 ml-5 hover:bg-[#5E2E53]">
+          <Link to={"/search"} className="border border-black rounded-full p-2 ml-5 hover:bg-white">
             <AiOutlineSearch className="text-black text-2xl" />
           </Link>
-          <Link to={"/calandar"} className="border border-black rounded-full p-2 ml-5 hover:bg-[#5E2E53]">
+          <Link to={"/calandar"} className="border border-black rounded-full p-2 ml-5 hover:bg-white">
             <AiOutlineCalendar className="text-black text-2xl" />
           </Link>
-          <Link to={"/logind"} className="border border-black rounded-full p-2 ml-5 hover:bg-[#5E2E53]">
+          <Link to={"/logind"} className="border border-black rounded-full p-2 ml-5 hover:bg-white">
             <AiOutlineUser className="text-black text-2xl" />
           </Link>
         </div>
